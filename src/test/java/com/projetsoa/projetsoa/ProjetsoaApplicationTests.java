8b3d1e2050db1341fe31c0b355c8da4d2d1c98ce
aliCh10 <130141511+aliCh10@users.noreply.github.com>
2023-10-24 23:05:22 +0100

    first commit

---
+++ src/test/java/com/projetsoa/projetsoa/ProjetsoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetsoa.projetsoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetsoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
